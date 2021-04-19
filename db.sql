a63f267cd3a1064aadea816891d5d238355c2e47
jinuk-choi <81605147+jinuk-choi@users.noreply.github.com>
2021-04-19 12:03:57 +0900

    comment2

--- db.sql
+++ db.sql
@@ -16,64 +16,38 @@
 CREATE DATABASE IF NOT EXISTS `aa` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `aa`;
 
--- 테이블 aa.board 구조 내보내기
-CREATE TABLE IF NOT EXISTS `board` (
-  `a_idx` int(10) NOT NULL AUTO_INCREMENT,
-  `a_writer` varchar(255) NOT NULL,
-  `a_title` varchar(255) NOT NULL,
-  `a_count` int(10) NOT NULL,
-  `a_content` text DEFAULT NULL,
-  PRIMARY KEY (`a_idx`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+-- 테이블 aa.comment 구조 내보내기
+CREATE TABLE IF NOT EXISTS `comment` (
+  `b_idx` int(11) NOT NULL AUTO_INCREMENT,
+  `b_name` varchar(50) DEFAULT NULL,
+  `b_title` varchar(50) DEFAULT NULL,
+  `b_content` varchar(50) DEFAULT NULL,
+  `a_idx` int(11) NOT NULL,
+  `b_redate` date DEFAULT current_timestamp(),
+  PRIMARY KEY (`b_idx`),
+  KEY `a_idx` (`a_idx`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 aa.board:~12 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `board` DISABLE KEYS */;
-INSERT INTO `board` (`a_idx`, `a_writer`, `a_title`, `a_count`, `a_content`) VALUES
-	(1, '홍길동', 'aaa', 0, 'ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ'),
-	(2, '이길동', 'bbb', 2, 'ㄴㄴㄴ'),
-	(3, '김김김', 'ccccc', 0, 'ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ'),
-	(4, 'aa', 'abc', 5, 'ㅊㅊㅊㅊㅊㅊㅊㅊㅊㅊ'),
-	(5, 'bbb', 'ddddd', 5, 'ㅇㅇ'),
-	(6, 'cccc', 'eefsf', 5, 'ㅋㅋㅋㅋㅋ'),
-	(7, 'ddddd', 'fefdsf', 5, 'ㅂㅂㅂㅂㅂㅂㅂ'),
-	(8, 'eeeeee', 'dsfaef', 5, 'ㅅㅇㅇㅇ'),
-	(9, 'gggggg', 'dsfaef', 5, 'ㅇㅇㅇ'),
-	(10, 'hnngg', 'dsfaef', 5, 'ㅋㅋㅋ'),
-	(11, 'zcc', 'sdfe', 5, 'ㅌㅌㅌ'),
-	(12, 'dbbb', 'dfsdfs', 5, 'ㅍㅍㅍㅍㅍㅍ');
-/*!40000 ALTER TABLE `board` ENABLE KEYS */;
-
--- 테이블 aa.user 구조 내보내기
-CREATE TABLE IF NOT EXISTS `user` (
-  `u_idx` int(10) NOT NULL AUTO_INCREMENT,
-  `u_id` varchar(255) NOT NULL,
-  `u_pw` varchar(255) NOT NULL,
-  `u_name` varchar(255) NOT NULL,
-  `u_tel` varchar(255) NOT NULL,
-  `u_age` int(11) DEFAULT NULL,
-  PRIMARY KEY (`u_idx`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
-
--- 테이블 데이터 aa.user:~15 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`u_idx`, `u_id`, `u_pw`, `u_name`, `u_tel`, `u_age`) VALUES
-	(3, 'a', '111', '김떙땡', '010-1111-1111', 200),
-	(4, 'b', '222', '이모씨', '010-2222-2222', 37),
-	(5, 'ccc', '34343', '강하다', '010-2121-3232', 58),
-	(6, 'dfdf', '545', '홍길동', '010-2898-4767', 44),
-	(7, 'zyzy', '252d', '대한민국', '010-4747-3634', 88),
-	(10, 'wew', 'qqqq', '박찬호', '010-7777-3232', 23),
-	(11, 'a1', '5555', '강호동', '090-4343-4444', 32),
-	(12, 'b1', '1111', '박길동', '090-3333-7777', 49),
-	(13, 'bb1', '2222', '고길동', '090-4444-6666', 61),
-	(14, 'cc3', 'c1c1', '최길동', '090-1111-3333', 74),
-	(15, 'aaaa', '1234', 'asdf', '1234-1234-4123', 34),
-	(16, 'lcomputer', '1234', '엘컴퓨터', '1111-1111-1111', 34),
-	(17, 'lcomputer12345', '1234', '엘컴터222', '010-5555-5555', 15),
-	(18, 'as2', '111', 'as2', '010', 12),
-	(19, 'as3', '111', 'as3', '011', 13),
-	(20, 'as4', '111', 'as4', '012', 14);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+-- 테이블 데이터 aa.comment:~16 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` (`b_idx`, `b_name`, `b_title`, `b_content`, `a_idx`, `b_redate`) VALUES
+	(1, '홍길동', 'ㅎㅇ', '안녕', 5, '2021-04-19'),
+	(2, '베스', 'ㅎㅇ', '안녕', 5, '2021-04-22'),
+	(3, '블루길', 'ㅎㅇ', '안녕', 5, '2021-04-11'),
+	(4, '붕어', '잘가', 'ㅂㅂㅂㅂ', 6, '2021-04-18'),
+	(5, '잉어', '가즈아', 'ㅇㅇ', 6, '2021-04-15'),
+	(6, '메기', 'ㅎㅇ', '안녕', 6, '2021-04-19'),
+	(7, '홍길동', 'ㅎㅇ', '하세', 6, '2021-04-19'),
+	(8, '홍길동', 'ㅎㅇ', '안녀엉', 7, '2021-04-19'),
+	(9, '홍길동', 'ㅂ2', '안녕', 7, '2021-04-19'),
+	(10, '홍길동', 'ㅎㅇ', '안녕', 7, '2021-04-19'),
+	(11, '홍길동', 'ㅂㅂ2', '안녕', 7, '2021-04-19'),
+	(12, '엘컴터', '안녕', '잘가', 0, NULL),
+	(13, '엘컴터', 'ㅇ', 'ㅇㅇ', 0, NULL),
+	(14, '가즈아', '가자', '가즈아', 5, NULL),
+	(15, '가물치', '잘가라', '잘가가', 5, NULL),
+	(16, '향어', '오랜만', 'ㅂㅂ2', 5, '2021-04-19');
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
